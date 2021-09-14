4e7e54ca1b5e66b29d89d07244e34c2fdf17d739
sayakanoa <reiluv75418@gmail.com>
2021-09-14 02:12:21 +0000

    destroy追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,10 +21,21 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
-  private
-  def book_params
-    params.permit(:title, :body)
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path(book)
   end
 
 
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
